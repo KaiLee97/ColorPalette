d584646fe4135db1090fd9af10b02324984acfe4
Kai Lee <haesong97@gmail.com>
2023-04-21 00:11:58 -0500

    Initial Commit

---
+++ ColorPalette/ColorPaletteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPaletteApp.swift
+//  ColorPalette
+//
+//  Created by Kai Lee on 4/21/23.
+//
+
+import SwiftUI
+
+@main
+struct ColorPaletteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
